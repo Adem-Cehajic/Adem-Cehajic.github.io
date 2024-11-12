26425d59ae69a7f52e3471d5919a3532208e8b99
Adem Cehajic <ademc1191@gmail.com>
2024-11-12 16:26:16 -0500

    Added Javascript and Css files
    I made the CSS its own file rather then the inline that was previously there so that it could be used for the projects file which Im working on. Also I used Javascript to have the color of the buttons change when being hovered over.

---
+++ javascript.js
@@ -0,0 +1,11 @@
+const buttons = document.querySelectorAll('.but'); 
+//list of all the buttons on the page, but not actual button elements the buttons I made to change websites
+buttons.forEach( (button) => {
+    button.addEventListener("mouseover", () => {
+        button.style.backgroundColor = "rgb(0,93,93)";
+    })
+    button.addEventListener("mouseleave", () => {
+        button.style.backgroundColor = "rgb(8,150,150)";
+    })
+})
+//goes through the list of buttons and gives them 2 events, one makes it darker when its being hovered over, the over changes it back
