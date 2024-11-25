f74609d6de6b0f81135568e8e934d6921961f2d2
Adem Cehajic <ademc1191@gmail.com>
2024-11-25 00:14:06 -0500

    Added projects
    I changed the formatting of the page aswell as added a box to indicate my calculator project.

--- javascript.js
+++ javascript.js
@@ -5,7 +5,7 @@ buttons.forEach( (button) => {
         button.style.backgroundColor = "rgb(0,93,93)";
     })
     button.addEventListener("mouseleave", () => {
-        button.style.backgroundColor = "rgb(8,150,150)";
+        button.style.backgroundColor = "#048C7F";
     })
 })
 //goes through the list of buttons and gives them 2 events, one makes it darker when its being hovered over, the over changes it back
