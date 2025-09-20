86a767658d4f0dbd6e45894110b3f2dbef9b2890
Adem-Cehajic <cehajica@mcmaster.ca>
2025-09-19 23:50:22 -0400

    updated a bit

--- javascript.js
+++ javascript.js
@@ -2,10 +2,10 @@ const buttons = document.querySelectorAll('.but');
 //list of all the buttons on the page, but not actual button elements the buttons I made to change websites
 buttons.forEach( (button) => {
     button.addEventListener("mouseover", () => {
-        button.style.backgroundColor = "rgb(0,93,93)";
+        button.style.backgroundColor = "#555a86ff";
     })
     button.addEventListener("mouseleave", () => {
-        button.style.backgroundColor = "#048C7F";
+        button.style.backgroundColor = "#384E77";
     })
 })
 //goes through the list of buttons and gives them 2 events, one makes it darker when its being hovered over, the over changes it back
